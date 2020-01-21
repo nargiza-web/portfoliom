9827e614e1af13d01b8d726dd2fcfa7e8976d716
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-01-21 15:01:44 -0600

    updated fav-icons and aboutme page

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.manu-nav');
+const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
@@ -12,14 +12,14 @@ function toggleMenu(){
   if(!showMenu){
     hamburger.classList.add('open');
     nav.classList.add('open');
-    manuNav.classList.add('open');
+    menuNav.classList.add('open');
     navItems.forEach(item => item.classList.add('open'));
     
     showMenu = true;
   } else {
     hamburger.classList.remove('open')
     nav.classList.remove('open');
-    manuNav.classList.remove('open');
+    menuNav.classList.remove('open');
     navItems.forEach(item => item.classList.remove('open'));
     
     showMenu = false;
